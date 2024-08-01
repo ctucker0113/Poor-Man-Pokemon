5f1bb19793d08c7307583a8021b4e44f026ae571
ctucker0113 <134985404+ctucker0113@users.noreply.github.com>
2024-08-01 10:43:29 -0500

    File organization. Created Pokemon class and sample data.

---
+++ Data/EmptyClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Poor_Man_Pokemon.Models; // Reference to the Pokemon model
+
+namespace Poor_Man_Pokemon.Data
+{
+    public class PokemonSampleData
+    {
+        public static List<Pokemon> Pokedex = new List<Pokemon>
+        {
+            new Pokemon
+            {
+                ID = 1,
+                Name = "Bulbasaur",
+                Type = "Grass",
+                HP = 40
+            },
+
+            new Pokemon
+            {
+                ID = 2,
+                Name = "Charmander",
+                Type = "Fire",
+                HP = 40
+            },
+
+            new Pokemon
+            {
+                ID = 3,
+                Name = "Squirtle",
+                Type = "Water",
+                HP = 40
+            },
+
+            new Pokemon
+            {
+                ID = 25,
+                Name = "Pikachu",
+                Type = "Electric",
+                HP = 40
+            },
+        };
+    }
+}
---
+++ Models/PokemonModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Poor_Man_Pokemon.Models
+{
+    public class Pokemon
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int HP { get; set; }
+    }
+}
